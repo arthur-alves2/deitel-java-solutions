c8dd493463767e0479467cad3273bb56ef8d597f
Arthur Alves <251051275+arthur-alves2@users.noreply.github.com>
2026-01-11 11:29:08 -0300

    feat(chapter02): solve exercise 29

---
+++ chapter02/exercise29/CharacterToInteger.java
@@ -0,0 +1,24 @@
+// This program shows the integer value of the characters A, B, C, a, b, c, 0, 1, 2, $, *, +, /
+// Author: Arthur Alves
+
+package chapter02.exercise29;
+
+public class CharacterToInteger {
+    public static void main(String[] args) {
+        System.out.printf("The character %c has the value %d%n", 'A', ((int) 'A'));
+        System.out.printf("The character %c has the value %d%n", 'B', ((int) 'B'));
+        System.out.printf("The character %c has the value %d%n", 'C', ((int) 'C'));
+        System.out.printf("The character %c has the value %d%n", 'a', ((int) 'a'));
+        System.out.printf("The character %c has the value %d%n", 'b', ((int) 'b'));
+        System.out.printf("The character %c has the value %d%n", 'c', ((int) 'c'));
+        System.out.printf("The character %c has the value %d%n", '0', ((int) '0'));
+        System.out.printf("The character %c has the value %d%n", '1', ((int) '1'));
+        System.out.printf("The character %c has the value %d%n", '2', ((int) '2'));
+        System.out.printf("The character %c has the value %d%n", '$', ((int) '$'));
+        System.out.printf("The character %c has the value %d%n", '*', ((int) '*'));
+        System.out.printf("The character %c has the value %d%n", '+', ((int) '+'));
+        System.out.printf("The character %c has the value %d%n", '/', ((int) '/'));
+        System.out.printf("The character %c has the value %d%n", ' ', ((int) ' '));
+    }
+}
+
